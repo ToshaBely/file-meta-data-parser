994ed478aab547af52789e6b58b344fa16dce983
Anton Bely <toshabely@gmail.com>
2026-02-15 14:33:49 +0100

    Start accepting files through API

---
+++ src/common/constants/files.constants.ts
@@ -0,0 +1,2 @@
+// Common Limits - many courts set a 10 MB to 35 MB limit for individual PDF files
+export const MAX_EXPECTED_CASE_LAW_FILE_SIZE = 35 * 1000 * 1000; // 35 MB in bytes
--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,16 +1,25 @@
 import {
   Controller,
+  FileTypeValidator,
   Get,
+  MaxFileSizeValidator,
   Param,
+  ParseFilePipe,
   ParseUUIDPipe,
   Post,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { NotImplementedException } from '@nestjs/common/exceptions/not-implemented.exception';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 import { RequiredResourcePermissions } from '../common/decorators/required-resource-permissions.decorator';
+
 import { AuthGuard } from '../common/guards/auth.guard';
 import { RequiredResourcePermissionsGuard } from '../common/guards/required-resource-permissions.guard';
+
+import { MAX_EXPECTED_CASE_LAW_FILE_SIZE } from '../common/constants/files.constants';
 
 @Controller('documents')
 @UseGuards(AuthGuard)
@@ -34,8 +43,28 @@ export class DocumentsController {
   }
 
   @Post('metadata\\:parse')
+  @UseInterceptors(FileInterceptor('file'))
   @RequiredResourcePermissions(['documents:admin'])
-  createDocument() {
+  parseDocumentMetadata(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({
+            fileType: /^(text\/html|application\/pdf)$/i,
+            // more info about "magic numbers" and default to mimetype:
+            // https://stackoverflow.com/questions/79792873/validating-file-type-by-nestjs-with-uploadedfile-decorator
+            fallbackToMimetype: true,
+            errorMessage: 'Validation failed - Expected file: HTML or PDF',
+          }),
+          new MaxFileSizeValidator({
+            maxSize: MAX_EXPECTED_CASE_LAW_FILE_SIZE,
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log('[parseDocumentMetadata] check file:', file);
     throw new NotImplementedException();
   }
 }
